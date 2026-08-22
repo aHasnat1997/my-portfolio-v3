6f46a9828136d32342bbe25cfb41717aad4c2bbc
aHasnat1997 <ahasnat557@gmail.com>
2026-08-22 11:03:50 +0600

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 import cloudflare from "@astrojs/cloudflare"
 
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel"
 
 export default defineConfig({
   vite: {
