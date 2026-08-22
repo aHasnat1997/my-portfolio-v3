495c8c0e82f68f4ee21dd7a194af7733d8fb7123
aHasnat1997 <ahasnat557@gmail.com>
2026-08-22 10:06:55 +0600

    ADD: supabase

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
-const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY
+const supabaseUrl =
+  import.meta.env.PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL
+const supabaseKey =
+  import.meta.env.PUBLIC_SUPABASE_KEY || process.env.SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
